5fe298bceda879beca25682513d747512baf1235
Yi Zhang <yzhan257@hawk.iit.edu>
2021-08-04 17:24:56 -0500

    SIR

--- FuntionPlotter.py
+++ FuntionPlotter.py
@@ -8,14 +8,29 @@ beta = 0.5
 
 
 def I_t(S_t):
-    return I_0 + gamma / beta * np.log(S_t / S_0) - (S_t - S_0)
+	return I_0 + gamma / beta * np.log(S_t / S_0) - (S_t - S_0)
+
+
+def tests():
+	print('I_0=', I_t(S_0))
+	print('S_peak=', gamma / beta)
+	print('I_peak=', I_t(gamma / beta))
+	S_range = np.arange(S_0, 0, -0.00001)
+	# print(S_range)
+	Is = I_t(S_range)
+	# print(Is)
+	for i in range(1, len(S_range)):
+		if Is[i] <= I_0:
+			print('S=', S_range[i], 'I=', Is[i])
+			break
+	print(i, len(S_range))
+	return
 
 
 def main():
-    print('S_peak=', gamma / beta)
-    print('I_peak=', I_t(gamma / beta))
-    return
+	tests()
+	return
 
 
 if __name__ == '__main__':
-    main()
+	main()
