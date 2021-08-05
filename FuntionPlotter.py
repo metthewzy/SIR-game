a5c6538b6be14b64053fd75afa043cd30d8a4f67
Yi Zhang <yzhan257@hawk.iit.edu>
2021-08-05 00:25:22 -0500

    SIRGame

--- FuntionPlotter.py
+++ FuntionPlotter.py
@@ -1,5 +1,5 @@
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 S_0 = 1
 I_0 = 0.0001
@@ -29,7 +29,7 @@ def tests():
 	while True:
 		S_middle = (S_left + S_right) / 2
 		I_middle = I_t(S_middle)
-		if abs(I_middle - I_0) / I_0 < 0.001:
+		if abs(I_middle - I_0) / I_0 < 0.0001:
 			break
 		if I_middle < I_0:
 			S_left = S_middle
@@ -38,12 +38,41 @@ def tests():
 	S_end = S_middle
 	I_end = I_t(S_end)
 	print(f'S_end={S_end} I_end={I_end}')
+	S_step = (S_0 - S_end) / 100000
+	S_range = np.arange(S_0, S_end - S_step, - S_step)
+	dts = del_t(S_range)
+	print(np.mean(dts) * (S_end - S_0))
+	return
 
+
+def del_t(S):
+	value = - 1 / (beta * S * I_0 + gamma * S * np.log(S) - beta * S ** 2 + 1)
+	return value
+
+
+def simulate():
+	S = [S_0]
+	I = [I_0]
+	dt = 0.01
+	t = np.arange(0, 10000.5 * dt, dt)
+	for i in range(10000):
+		dS = (- beta * S[-1] * I[-1]) * dt
+		dI = (beta * S[-1] - gamma) * I[-1] * dt
+		S.append(S[-1] + dS)
+		I.append(I[-1] + dI)
+
+	fig = plt.figure()
+	ax = fig.add_subplot()
+	ax.plot(t, S, label='S')
+	ax.plot(t, I, label='I')
+	ax.legend()
+	plt.show()
 	return
 
 
 def main():
 	tests()
+	simulate()
 	return
 
 
