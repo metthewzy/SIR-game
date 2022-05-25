e4387c929f3a8684bc5aeaa5506b6a2124a244be
metth <yzhan257@hawk.iit.edu>
2022-05-25 17:58:53 -0500

    simulation with interaction

--- FuntionPlotter.py
+++ FuntionPlotter.py
@@ -60,6 +60,7 @@ def del_s_by_del_t(S):
 	return value
 
 
+# 1 group SIR simulation
 def simulate(beta, S0, I0, t_vac, showPlot):
 	S = [S0]
 	I = [I0 * S0]
@@ -80,6 +81,43 @@ def simulate(beta, S0, I0, t_vac, showPlot):
 		plt.show()
 		plt.close(fig)
 	return S, I, t_range
+
+
+# 2 groups simulation with interactions in between
+def simulate_interaction(beta1, S1_0, beta2, S2_0, I_0, t_vac, showPlot):
+	S1 = [S1_0]
+	I1 = [I_0 * S1_0]
+	S2 = [S2_0]
+	I2 = [I_0 * S2_0]
+	dt = 0.01
+	t_range = np.arange(0, t_vac + dt, dt)
+	for t in t_range[1:]:
+		# dS1 = (- S1[-1] * (beta1 * I1[-1] + beta2 * I2[-1])) * dt
+		# dI1 = (S1[-1] * (beta1 * I1[-1] + beta2 * I2[-1]) + gamma * I1[-1]) * dt
+		dS1 = (- beta1 * S1[-1] * (I1[-1] + I2[-1])) * dt
+		dI1 = (beta1 * S1[-1] * (I1[-1] + I2[-1]) + gamma * I1[-1]) * dt
+
+		# dS2 = (- S2[-1] * (beta1 * I1[-1] + beta2 * I2[-1])) * dt
+		# dI2 = (S2[-1] * (beta1 * I1[-1] + beta2 * I2[-1]) + gamma * I2[-1]) * dt
+		dS2 = (- beta2 * S2[-1] * (I1[-1] + I2[-1])) * dt
+		dI2 = (beta2 * S2[-1] * (I1[-1] + I2[-1]) + gamma * I2[-1]) * dt
+
+		S1.append(S1[-1] + dS1)
+		I1.append(I1[-1] + dI1)
+		S2.append(S2[-1] + dS2)
+		I2.append(I2[-1] + dI2)
+
+	if showPlot:
+		fig = plt.figure()
+		ax = fig.add_subplot()
+		ax.plot(t_range, S1, label='S1')
+		ax.plot(t_range, I1, label='I1')
+		ax.plot(t_range, S2, label='S2')
+		ax.plot(t_range, I2, label='I2')
+		ax.legend()
+		plt.show()
+		plt.close(fig)
+	return S1, I1, S2, I2, t_range
 
 
 def simulate_scaled(beta, S0, I0, t_vac, dt, showPlot):
@@ -142,8 +180,8 @@ def dU_by_dt(income, beta, S_t, I_t, S0, t, t_vac):
 	return value
 
 
-# 2 group game. S:susceptible, M:mask. plotting the player's expected utility in each group, and the social utility
-# with varying size of group S
+# 2 group game w/o interaction. S:susceptible, M:mask. plotting the player's expected utility in each group,
+# and the social utility with varying size of group S
 def utility_plotter(income_ratio, beta_ratio, t_vac):
 	U_S = []
 	U_M = []
@@ -156,22 +194,25 @@ def utility_plotter(income_ratio, beta_ratio, t_vac):
 	beta_S = beta_0
 	beta_M = beta_0 * beta_ratio
 	step_size = 0.005
-	S0_S_range = np.arange(0, 1 + step_size, step_size)
+	# S0_S_range = np.arange(0, 1 + step_size, step_size)
+	S0_S_range = np.arange(0 + step_size, 1, step_size)
 	for S0_S in S0_S_range:
 		SS, IS, t_range = simulate(beta_S, S0_S, I_0, t_vac, False)
 		# susceptible group utility
 		SS_list.append(GDP1 * np.mean(SS) * t_vac)
 		# player's expected utility in susceptible group
-		U_S.append(np.mean(
-			[dU_by_dt(GDP1, beta_S, SS[i], IS[i], S0_S, t_range[i], t_vac) for i in range(len(t_range))]) * t_vac)
+		# U_S.append(np.mean(
+		# 	[dU_by_dt(GDP1, beta_S, SS[i], IS[i], S0_S, t_range[i], t_vac) for i in range(len(t_range))]) * t_vac)
+		U_S.append(GDP1 * t_vac if S0_S == 0 else SS_list[-1] / S0_S)
 
 		S0_M = 1 - S0_S
 		SM, IM, t_range = simulate(beta_M, S0_M, I_0, t_vac, False)
 		# mask group utility
 		SM_list.append(GDP2 * np.mean(SM) * t_vac)
 		# player's expected utility in mask group
-		U_M.append(np.mean(
-			[dU_by_dt(GDP2, beta_M, SM[i], IM[i], S0_M, t_range[i], t_vac) for i in range(len(t_range))]) * t_vac)
+		# U_M.append(np.mean(
+		# 	[dU_by_dt(GDP2, beta_M, SM[i], IM[i], S0_M, t_range[i], t_vac) for i in range(len(t_range))]) * t_vac)
+		U_M.append(GDP2 * t_vac if S0_M == 0 else SM_list[-1] / S0_M)
 
 		socialU.append(SS_list[-1] + SM_list[-1])
 
@@ -589,10 +630,94 @@ def POA_grid():
 	return
 
 
+# 2 group game with interaction. S:susceptible, M:mask. plotting the player's expected utility in each group,
+# and the social utility with varying size of group S
+def utility_plotter_interaction(income_ratio, beta_ratio, t_vac):
+	U_S = []
+	U_M = []
+	SS_list = []
+	SM_list = []
+	socialU = []
+	# susceptible group daily payment. mask group daily payment assumed to be 1
+	GDP1 = income_ratio
+	GDP2 = 1
+	beta_S = beta_0
+	beta_M = beta_0 * beta_ratio
+	step_size = 0.005
+	# S0_S_range = np.arange(0, 1 + step_size, step_size)
+	S0_S_range = np.arange(0 + step_size, 1, step_size)
+	for S0_S in S0_S_range:
+		S0_M = 1 - S0_S
+		SS, IS, SM, IM, t_range = simulate_interaction(beta_S, S0_S, beta_M, S0_M, I_0, t_vac, False)
+
+		# susceptible group utility
+		SS_list.append(GDP1 * np.mean(SS) * t_vac)
+		# player's expected utility in susceptible group
+		U_S.append(GDP1 * t_vac if S0_S == 0 else SS_list[-1] / S0_S)
+
+		# mask group utility
+		SM_list.append(GDP2 * np.mean(SM) * t_vac)
+		# player's expected utility in mask group
+		U_M.append(GDP2 * t_vac if S0_M == 0 else SM_list[-1] / S0_M)
+
+		socialU.append(SS_list[-1] + SM_list[-1])
+
+	fig = plt.figure()
+	ax1 = fig.add_subplot(221)
+	# ax1.plot(S0_S_range, U_S, label='U(Player)_S')
+	# ax1.plot(S0_S_range, U_M, label='U(Player)_M')
+	ax1.plot(S0_S_range, SS_list, label='Group Utility s')
+	ax1.plot(S0_S_range, SM_list, label='Group Utility m')
+	ax1.plot(S0_S_range, socialU, label='social')
+	max_social = max(socialU)
+	maxIndex = socialU.index(max_social)
+	ax1.axhline(max_social, linestyle=':', label=f'OPT={round(max_social, 4)}', color='red')
+	ax1.axvline(S0_S_range[maxIndex], linestyle=':', color='red', label=f'OPT@{round(S0_S_range[maxIndex], 4)}')
+	ax1.set_xlabel('susceptible size')
+	ax1.set_ylabel('utility')
+	ax1.legend()
+
+	# dGroup utility by d phi(S)
+	ax3 = fig.add_subplot(223)
+	dGroup_utility_S = [SS_list[i] - SS_list[i - 1] for i in range(1, len(SS_list))]
+	ax3.plot(S0_S_range[1:], dGroup_utility_S, label='dUG(S)')
+
+	dGroup_utility_M = [SM_list[i] - SM_list[i - 1] for i in range(1, len(SM_list))]
+	ax3.plot(S0_S_range[1:], dGroup_utility_M, label='dUG(M)')
+
+	sum_dUG = [dGroup_utility_S[i] + dGroup_utility_M[i] for i in range(len(dGroup_utility_S))]
+	ax3.plot(S0_S_range[1:], sum_dUG, label='sum dUG')
+	# ax3.axhline(sum_dUG[maxIndex], linestyle=':', label=f'OPT={round(sum_dUG[maxIndex], 4)}', color='red')
+	ax3.axvline(S0_S_range[maxIndex], linestyle=':', color='red', label=f'OPT@{round(S0_S_range[maxIndex], 4)}')
+	ax3.legend()
+
+	# search for the NE point
+	NE_S0_S_range, NE_U_S, NE_U_M, NE_utility = NE_searcher(t_vac, GDP1, GDP2, beta_S, beta_M)
+	NE_S0_S = NE_S0_S_range[-1]
+	POA = max_social / NE_utility
+	print('POA=', POA)
+	# NE_S0_S_range, NE_U_S, NE_U_M = map(list, zip(*sorted(zip(NE_S0_S_range, NE_U_S, NE_U_M))))
+	ax2 = fig.add_subplot(222)
+	# ax2.plot(NE_S0_S_range, NE_U_S, label='U(Player)_S')
+	# ax2.plot(NE_S0_S_range, NE_U_M, label='U(Player)_M')
+	ax2.plot(S0_S_range, U_S, label='U(Player)_S')
+	ax2.plot(S0_S_range, U_M, label='U(Player)_M')
+	ax2.axhline(NE_utility, label=f'NE={round(NE_utility, 4)}', linestyle=':', color='red')
+	ax2.axvline(NE_S0_S, label=f'NE@{round(NE_S0_S, 4)}', linestyle=':', color='red')
+	ax2.set_xlabel('susceptible size')
+	ax2.set_ylabel('utility')
+	ax2.legend()
+	fig.suptitle(f'POA={round(max_social / NE_utility, 4)}')
+	plt.show()
+	plt.close(fig)
+	return
+
+
 def main():
 	# tests()
 
-	utility_plotter(income_ratio=6, beta_ratio=0.5, t_vac=100)
+	# utility_plotter(income_ratio=6, beta_ratio=0.5, t_vac=100)
+	utility_plotter_interaction(income_ratio=2, beta_ratio=0.5, t_vac=100)
 	# POA_grid()
 
 	# curvePlotter()
