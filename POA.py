fe607b612a257b1920974fabd775995c47e77633
metth <yzhan257@hawk.iit.edu>
2022-06-21 15:30:14 -0500

    t_peak evaluated from 2/3 S_0

--- POA.py
+++ POA.py
@@ -30,7 +30,7 @@ def t_peak_comparison():
 	I0_global = 0.0001
 	t_vac = 100
 	gamma = 1 / 14
-	beta = 0.2
+	beta = 1
 	I0 = I0_global * S0
 	S, I, t_range = simulate(beta, gamma, S0, I0_global, t_vac, False)
 	fig = plt.figure()
@@ -39,30 +39,61 @@ def t_peak_comparison():
 	ax1.plot(t_range, I, label='I')
 
 	S_peak = gamma / beta
-	S_peak_idx = [i for i in range(len(S)) if S[i] >= S_peak][-1]
-	t_peak = t_range[S_peak_idx]
-	ax1.vlines(x=t_peak, ymin=0, ymax=S_peak, label='t_peak', color='red', linestyle=':')
+	# S_peak_idx = [i for i in range(len(S)) if S[i] >= S_peak][-1]
+	# t_peak = t_range[S_peak_idx]
+	# ax1.vlines(x=t_peak, ymin=0, ymax=S_peak, label='t_peak', color='red', linestyle=':')
 
-	C1 = S0 / (gamma - beta * S0)
-	C2 = (beta * (I0 + S0) - gamma) * S0 / (gamma - beta * S0)
-	t_first_half = - C1 / C2 * np.log((S0 / 2) / (S0 / 2 + C2) * (S0 + C2) / S0)
-	C1 = S0 / (beta * S0 - 2 * gamma)
-	C2 = gamma - gamma * np.log(0.5) - beta * I0 - beta * S0
-	t_second_half = 1 / C2 * np.log((gamma / beta) / (gamma / beta + C1 * C2) * (S0 / 2 + C1 * C2) / (S0 / 2))
+	# t_first_half = t_first(S0, S0 / 2, beta, gamma, S0, I0)
+	# t_second_half = t_second(S0 / 2, S_peak, beta, gamma, S0, I0)
+
+	t_first_half = t_first(S0, S0 / 3 * 2, beta, gamma, S0, I0)
+	t_second_half = t_second(S0 / 3 * 2, S_peak, beta, gamma, S0, I0)
+
 	t_peak_est = t_first_half + t_second_half
+
 	ax1.vlines(x=t_peak_est, ymin=0, ymax=S_peak, label='t_peak_est', color='blue', linestyle=':')
 
-	ax1.vlines(x=t_first_half, ymin=0, ymax=S0 / 2, color='grey')
-	ax1.hlines(y=S0 / 2, xmin=0, xmax=t_first_half, color='grey')
-	ax1.hlines(y=S_peak, xmin=t_first_half, xmax=t_peak, label='S_peak', color='grey', linestyle=':')
-	print(t_first_half)
+	# ax1.vlines(x=t_first_half, ymin=0, ymax=S0 / 2, color='grey')
+	# ax1.hlines(y=S0 / 2, xmin=0, xmax=t_first_half, color='grey')
+
+	ax1.vlines(x=t_first_half, ymin=0, ymax=S0 / 3 * 2, color='grey')
+	ax1.hlines(y=S0 / 3 * 2, xmin=0, xmax=t_first_half, color='grey')
+
+	ax1.hlines(y=S_peak, xmin=t_first_half, xmax=t_vac, label='S_peak', color='grey', linestyle=':')
 
 	# ax1.set_xlim(0, 20)
 	ax1.set_title(f'beta={round(beta, 3)}')
 	ax1.legend()
+	S_area = np.mean(S) * t_vac
+	print(S_area)
+	# rectangle_area = S0 / 2 * t_first_half + S_peak * t_second_half
+	rectangle_area = S0 / 3 * 2 * t_first_half + S_peak * t_second_half
+	print(rectangle_area)
+	print(f'{round(rectangle_area / S_area * 100, 2)}% of the actual area')
 
 	plt.show()
 	return
+
+
+def t_first(S1, S2, beta, gamma, S0, I0):
+	C1 = S0 / (gamma - beta * S0)
+	C2 = (beta * (I0 + S0) - gamma) * S0 / (gamma - beta * S0)
+	ret = - C1 / C2 * np.log(S2 / (S2 + C2) * (S1 + C2) / S1)
+	return ret
+
+
+def t_second(S1, S2, beta, gamma, S0, I0):
+	C1 = S0 / (beta * S0 - 2 * gamma)
+	C2 = gamma - gamma * np.log(0.5) - beta * I0 - beta * S0
+	ret = np.log(S2 / (S2 + C1 * C2) * (S1 + C1 * C2) / S1) / C2
+	return ret
+
+
+def t_second_V2(S1, S2, beta, gamma, S0, I0):
+	C1 = 2 * S0 / (2 * beta * S0 - 3 * gamma)
+	C2 = gamma - gamma * np.log(2 / 3) - beta * I0 - beta * S0
+	ret = np.log(S2 / (S2 + C1 * C2) * (S1 + C1 * C2) / S1) / C2
+	return ret
 
 
 def main():
