79f22250fe50486ae39b72509494b4012aed5ecc
metth <yzhan257@hawk.iit.edu>
2022-11-14 16:06:54 -0600

    convex program

--- ConvexProgram.py
+++ ConvexProgram.py
@@ -1,16 +1,31 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import minimize
+from scipy.optimize import Bounds
+from scipy.optimize import NonlinearConstraint
 
 
 def OBJ(S):
+	"""
+	objective function
+	"""
 	ret = sum(S)
 	return ret
 
 
-def f_i(beta, gamma, kappa, epsilon, n, phi, S, i):
-	S_i = S[i]
-	phi_i = phi[i]
+def f_i(n, beta_M, gamma, epsilon, phi_V, S, i):
+	"""
+	constraint function f_i()
+	"""
+	X = sum([beta_M[i][j] / gamma * (S[j] - phi_V[j])
+			 for j in range(n)])
+	ret = S[i] - (1 - epsilon) * phi_V[i] * np.exp(X)
+	return ret
+
+
+def min_program(n, beta, gamma, epsilon, phi_V):
+	bounds = Bounds([(0, (1 - epsilon) * phi) for phi in phi_V])
+	constraints = NonlinearConstraint()
 	return
 
 
