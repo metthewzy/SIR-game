7e85838d86651ebccef25c8a55527cf6585aa78d
Yi Zhang <yzhan257@hawk.iit.edu>
2023-01-31 22:30:31 -0600

    convex program

--- ConvexProgram.py
+++ ConvexProgram.py
@@ -4,6 +4,18 @@ from scipy.optimize import minimize
 import cvxpy as cp
 
 
+def f(s, phi, betas, gamma, s_vec):
+	return
+
+
+def test():
+	R0 = 2
+	k = 0.8
+	s1 = cp.Variable()
+	s2 = cp.Variable()
+	return
+
+
 def main():
 	return
 
