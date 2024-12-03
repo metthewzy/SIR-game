c967be89fffc9da1c851a127f1ffe62c7034ee8c
mett <metthewzy@gmail.com>
2024-12-03 11:49:05 -0600

    d improvement / d budget

--- Incentive.py
+++ Incentive.py
@@ -8,9 +8,16 @@ import pandas as pd
 color_map = ['red', 'green', 'blue']
 
 
+def derivative(X, Y):
+    dYs = [Y[i] - Y[i - 1] for i in range(1, len(Y))]
+    dXs = [X[i] - X[i - 1] for i in range(1, len(X))]
+    dY_by_dX = [dY / dX for dY, dX in zip(dYs, dXs)]
+    return dY_by_dX
+
+
 def two_group_NE_finder(U1s, U2s):
     """
-    return phi1 value at NE
+    return index at NE
     """
     for i in range(len(U1s)):
         if U1s[i] <= U2s[i]:
@@ -19,7 +26,7 @@ def two_group_NE_finder(U1s, U2s):
 
 
 def two_group_incentive():
-    csv_name, paras = two_group_parameter_scenario(1)  # 0: low infection; 1: high infection
+    csv_name, paras = two_group_parameter_scenario(0)  # 0: low infection; 1: high infection
     # save_two_group_Sbar(csv_name, paras)
     plot_two_group(csv_name, paras)
     plot_two_group_incentive(csv_name, paras)
@@ -85,26 +92,41 @@ def plot_two_group_incentive(csv_name, paras):
     beta0, gamma, epsilon, k1, k2, ps = paras
     phi1s, Sbar1s, Sbar2s = read_two_group_Sbar(csv_name)
     p1, p2 = ps
+    U1s = [p1 * Sbar1 for Sbar1 in Sbar1s]
+    U2s = [p2 * Sbar2 for Sbar2 in Sbar2s]
+    UG1s = [U1 * phi1 for U1, phi1 in zip(U1s, phi1s)]
+    UG2s = [U2 * (1 - phi1) for U2, phi1 in zip(U2s, phi1s)]
+    NE_idx = two_group_NE_finder(U1s, U2s)
     socials = [p1 * phi1 * Sbar1 + p2 * (1 - phi1) * Sbar2 for phi1, Sbar1, Sbar2 in zip(phi1s, Sbar1s, Sbar2s)]
+    social_NE = socials[NE_idx]
     Delta1s, Delta2s, Bs = two_group_Delta_finder(csv_name, paras)
-    new_p1s = [p1 + Delta1 for Delta1 in Delta1s]
-    new_p2s = [p2 + Delta2 for Delta2 in Delta2s]
-    NEs = [max(p1 * Sbar1, p2 * Sbar2) for phi1, Sbar1, Sbar2 in zip(phi1s, Sbar1s, Sbar2s)]
-
-    # metric1: Social (new NE) / Budget
-    metric1 = [0 if B == 0 else NE / B for NE, B in zip(NEs, Bs)]
-    # metric2: Social improvement / Budget
-    metric2 = [0 if B == 0 else (NE - social) / B for NE, social, B in zip(NEs, socials, Bs)]
 
     fig = plt.figure()
-    ax1 = fig.add_subplot(121)
-    ax1.plot(phi1s, metric1, label='metric 1')
+    ax1 = fig.add_subplot(111)
+    ax1.plot(phi1s[:NE_idx], UG1s[:NE_idx], label='UG1')
+    ax1.plot(phi1s[:NE_idx], UG2s[:NE_idx], label='UG2')
     ax1.set_xlabel(r'$\phi_1$')
     ax1.legend()
+    plt.show()
+    # new_p1s = [p1 + Delta1 for Delta1 in Delta1s]
+    # new_p2s = [p2 + Delta2 for Delta2 in Delta2s]
+    new_NEs = [max(p1 * Sbar1, p2 * Sbar2) for phi1, Sbar1, Sbar2 in zip(phi1s, Sbar1s, Sbar2s)]
 
-    ax2 = fig.add_subplot(122)
-    ax2.plot(phi1s, metric2, label='metric 2')
-    ax2.set_xlabel(r'$\phi_1$')
+    # metric1: Social (new NE) / Budget
+    metric1 = [0 if B == 0 else NE / B for NE, B in zip(new_NEs, Bs)]
+    # metric2: Social improvement / Budget
+    metric2 = [0 if B == 0 else (NE - social_NE) for NE, B in zip(new_NEs, Bs)]
+
+    fig = plt.figure()
+    ax1 = fig.add_subplot(211)
+    ax1.plot(Bs[:NE_idx], [NE for NE, B in zip(new_NEs[:NE_idx], Bs[:NE_idx])], label='metric 1')
+    ax1.set_xlabel(r'Budget')
+    ax1.legend()
+    d_social_by_d_B = derivative(Bs[:NE_idx], new_NEs[:NE_idx])
+    ax2 = fig.add_subplot(212)
+    # ax2.plot(Bs[:NE_idx], [(NE) / B for NE, B in zip(new_NEs[:NE_idx], Bs[:NE_idx])], label='metric 2')
+    ax2.plot(Bs[1:NE_idx], d_social_by_d_B, label='d Social / d Budget')
+    ax2.set_xlabel(r'Budget')
     ax2.legend()
 
     plt.show()
@@ -269,7 +291,7 @@ def three_group_incentive():
     k2 = 0.8
     k3 = 0.7
 
-    ps = [1, 0.95, 0.918]
+    ps = [1, 0.95, 0.9259]
     paras = [beta0, gamma, epsilon, k1, k2, k3, ps]
 
     # save_three_group_Sbar(csv_name, paras)
@@ -279,8 +301,8 @@ def three_group_incentive():
 
 
 def main():
-    two_group_incentive()
-    # three_group_incentive()
+    # two_group_incentive()
+    three_group_incentive()
     return
 
 
