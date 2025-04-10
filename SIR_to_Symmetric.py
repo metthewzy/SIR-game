6dcec91be61505fd082cb82728a2c41913eac584
mett <metthewzy@gmail.com>
2025-04-10 18:32:03 -0500

    started SIR to symmetric

--- SIR_to_Symmetric.py
+++ SIR_to_Symmetric.py
@@ -1,4 +1,43 @@
+import numpy as np
+
+
+def reduce(support, beta, gamma, epsilon, p, N, n_groups):
+    NE = []
+    not_NE = []
+    for i in range(n_groups):
+        bit = 1 << i
+        if bit & support:
+            NE.append(i)
+        else:
+            not_NE.append(i)
+    print("NE:", NE)
+    print("not NE:", not_NE)
+    B = []
+    return True, []
+
+
+def three_groups():
+    n_groups = 3
+    beta = [
+        [1, 2, 3],
+        [3, 2, 1],
+        [1, 4, 5]
+    ]
+    gamma = 2
+    epsilon = 0.01
+    p = [0.5, 1, 0.8]
+    Ns = [0.5 * max(p)]
+    for N in Ns:
+        phi = None
+        for support in range(1, 2 ** n_groups):
+            res, phi = reduce(support, beta, gamma, epsilon, p, N, n_groups)
+            # if res:
+            #     break
+    return
+
+
 def main():
+    three_groups()
     return
 
 
